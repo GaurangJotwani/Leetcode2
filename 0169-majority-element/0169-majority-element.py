1e9364db76cd7a9efe209c8b8368bea4178d3255
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2023-09-20 22:01:20 -0700

    Time: 149 ms (69.34%), Space: 17.8 MB (92.36%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -3,21 +3,6 @@ class Solution:
         if len(nums) == 1: return nums[0]
         nums.sort()
 
-        i = 0
-        majority = ceil(len(nums) / 2)
-        print(majority)
-
-        while i < len(nums):
-
-            c_num = nums[i]
-            c_freq = 1
-            i += 1
-            while i < len(nums) and nums[i] == c_num:
-                i += 1
-                c_freq += 1
-                if c_freq >= majority:
-                    return c_num
-        
-        return 0
+        return nums[len(nums) // 2]
              
         
