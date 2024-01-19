ce00114c2aac4f91f26baa597959f76f69a7b02b
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-01-18 18:32:45 -0800

    Time: 8 ms (71.28%), Space: 12.3 MB (68.65%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> stk;
+        for(auto token: tokens) {
+            
+            if (token.size() > 1 || isdigit(token[0])) {
+                stk.push(stoi(token));
+                continue;
+            }
+            
+            int num2 = stk.top();
+            stk.pop();
+            int num1 = stk.top();
+            stk.pop();
+            int result;
+            
+            if (token == "+") {
+                result = num1 + num2;
+            } else if (token == "-") {
+                result = num1 - num2;
+            } else if (token == "*") {
+                result = num1 * num2;
+            } else {
+                result = num1 / num2;
+            }
+            stk.push(result);
+        }
+        return stk.top();
+    }
+};
