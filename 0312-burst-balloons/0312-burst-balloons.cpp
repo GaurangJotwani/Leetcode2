d4e71e5233802174c5d50352312db1de2ba14699
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-01-21 08:33:52 -0800

    Time: 162 ms (98.47%), Space: 11.9 MB (5.28%) - LeetHub

---
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>>dp(n, vector<int>(n));
+        
+        for (int g = 0; g < n; g++) {
+            
+            for (int i = 0, j = g; j < n; i++, j++) {
+                
+                int max = INT_MIN;
+                for (int k = i; k <= j; k++) {
+                    int left = k == i ? 0 : dp[i][k - 1];
+                    int right = k == j ? 0 : dp[k + 1][j];
+                    int val = (i == 0 ? 1 : nums[i - 1]) * nums[k] * (j == n - 1 ? 1 : nums[j + 1]);
+                    int total = left + right + val;
+                    if (total > max) max = total;
+                    
+                }
+                
+                dp[i][j] = max;
+            }
+        }
+        
+        
+        
+        
+        return dp[0][n - 1];
+    }
+};
