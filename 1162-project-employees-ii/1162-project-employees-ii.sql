edff8a320ed2abf7e352a7f69ce659ff004f7060
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-07-13 12:40:42 -0400

    Time: 874 ms (54.6%), Space: 0B (100%) - LeetHub

--- 1162-project-employees-ii/1162-project-employees-ii.sql
+++ 1162-project-employees-ii/1162-project-employees-ii.sql
@@ -1,14 +1,12 @@
-# Write your MySQL query statement below
-
-SELECT p.project_id
-FROM Project AS p
-JOIN Employee AS E ON p.employee_id = e.employee_id
-GROUP BY p.project_id
-HAVING COUNT(*) = (SELECT COUNT(*)
-FROM Project AS p
-JOIN Employee AS E ON p.employee_id = e.employee_id
-GROUP BY p.project_id
-ORDER BY COUNT(*) DESC
-LIMIT 1)
-
-
+WITH ProjectEmployeeCount AS (
+    SELECT p.project_id, COUNT(*) AS employee_count
+    FROM Project AS p
+    JOIN Employee AS E ON p.employee_id = e.employee_id
+    GROUP BY p.project_id
+)
+SELECT project_id
+FROM ProjectEmployeeCount
+WHERE employee_count = (
+    SELECT MAX(employee_count)
+    FROM ProjectEmployeeCount
+);
