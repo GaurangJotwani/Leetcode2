d5886252aedfa6dbf0ae3122bd363c5cf019f25f
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-07-13 12:44:29 -0400

    Time: 754 ms (75.58%), Space: 0B (100%) - LeetHub

--- 1162-project-employees-ii/1162-project-employees-ii.sql
+++ 1162-project-employees-ii/1162-project-employees-ii.sql
@@ -2,11 +2,22 @@ WITH ProjectEmployeeCount AS (
     SELECT p.project_id, COUNT(*) AS employee_count
     FROM Project AS p
     JOIN Employee AS E ON p.employee_id = e.employee_id
-    GROUP BY p.project_id
+    GROUP BY p.project_id 
 )
 SELECT project_id
 FROM ProjectEmployeeCount
-WHERE employee_count = (
-    SELECT MAX(employee_count)
-    FROM ProjectEmployeeCount
-);
+WHERE employee_count = (SELECT MAX(employee_count) FROM ProjectEmployeeCount)
+
+-- WITH ProjectEmployeeCount AS (
+--     SELECT p.project_id, COUNT(*) AS employee_count
+--     FROM Project AS p
+--     JOIN Employee AS E ON p.employee_id = e.employee_id
+--     GROUP BY p.project_id
+-- )
+-- SELECT project_id
+-- FROM ProjectEmployeeCount
+-- WHERE employee_count = (
+--     SELECT MAX(employee_count)
+--     FROM ProjectEmployeeCount
+-- );
+
