c4daea3527c0069a43701f3e1b9cb0cd23c1059d
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-07-26 15:22:30 -0400

    Time: 24 ms (12.41%), Space: 17.5 MB (17.08%) - LeetHub

---
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+vector<vector<int>> adjList;
+
+bool dfs(int node, int parent, vector<int> &seen, int color) {
+    seen[node] = color;
+    
+    for (auto nei: adjList[node]) {
+        if (seen[nei] == 0) {
+            if (!dfs(nei, node, seen, 3 - color)) return false;
+        }
+        else if(nei != parent && seen[nei] == color) return false;
+    }
+    
+    return true;
+}
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+    vector<int> seen(n, 0);
+    
+    adjList = graph;
+    
+    for (int i = 0; i < n; i++) {
+        if (seen[i] == 0) {
+            if (!dfs(i, -1, seen, 1)) return false;
+        }
+    }
+    
+    return true;
+    }
+};
