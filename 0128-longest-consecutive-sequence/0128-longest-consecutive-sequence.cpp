f541552d5e36b2a5ed3dfbe46dad7fec1e2bc3fd
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-04 16:41:56 -0400

    Time: 131 ms (46.71%), Space: 74.5 MB (47.02%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        
-        if (nums.size() == 0) return 0;
-        
         int res = 1;
-        unordered_map<int, bool>seen;
+        if (nums.size() == 0) return 0;
+        unordered_map<int, bool> seen;
+        for (auto num: nums) seen[num] = false;
+
         for (auto num: nums) {
-            seen[num] = false;
-        }
-        
-        for (auto num: nums) {
-            
-            if (seen[num] == true) continue;
+            if (seen[num]) continue;
             seen[num] = true;
-            
+
             int left = num - 1;
             while (seen.find(left) != seen.end()) {
                 seen[left] = true;
-                left -= 1;
+                left--;
             }
-            
             int right = num + 1;
             while (seen.find(right) != seen.end()) {
                 seen[right] = true;
-                right += 1;
+                right++;
             }
             res = max(res, right - left - 1);
         }
-        
         return res;
     }
 };
