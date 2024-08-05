e9ccf54b00c63d09c382228a997f4513257e0367
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-05 19:05:34 -0400

    Time: 3 ms (93.96%), Space: 8.7 MB (62.78%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int m = s1.size();
-        int n = s2.size();
+        int m = s1.size(), n = s2.size();
         if (m > n) return false;
-        
+
         vector<int> count(26);
         for (int i = 0; i < m; i++) {
             count[s1[i] - 'a']++;
             count[s2[i] - 'a']--;
         }
-        
-        if (isPermutation(count)) return true;
-        
+
+        if (isPerm(count)) return true;
         for (int i = m; i < n; i++) {
             count[s2[i] - 'a']--;
-            count[s2[i - m] - 'a']++;
-            if (isPermutation(count)) return true;
+            count[s2[i-m] - 'a']++;
+            if (isPerm(count)) return true;
         }
-        
+
         return false;
     }
-    
-private:
-    bool isPermutation(vector<int>& count) {
-        for (auto num: count) {
-            if (num != 0) return false;
-        }
+
+    bool isPerm(vector<int> &count) {
+        for (auto num: count) if (num != 0) return false;
         return true;
     }
+
 };
