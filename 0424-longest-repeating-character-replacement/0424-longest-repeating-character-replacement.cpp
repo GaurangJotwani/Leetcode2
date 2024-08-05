28dda6af00fcc827b6e635ee08cc844ef103987b
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-05 18:53:49 -0400

    Time: 3 ms (95.89%), Space: 8.5 MB (50.38%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        
         vector<int> count(26);
-        int maxCount = 0;
-        
-        int i = 0, j = 0;
-        int result = 0;
-        
+        int i = 0, j = 0, result = 0, maxCount = 0;
+
         while (j < s.size()) {
             count[s[j] - 'A']++;
             maxCount = max(maxCount, count[s[j] - 'A']);
@@ -18,7 +14,7 @@ public:
             result = max(result, j - i + 1);
             j++;
         }
-        
         return result;
+
     }
 };
