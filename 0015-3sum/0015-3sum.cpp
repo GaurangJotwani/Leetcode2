f177f84540b8076ec9262af0f54307149e121afe
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-05 11:07:51 -0400

    Time: 65 ms (57.07%), Space: 27.4 MB (65.45%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,8 +5,8 @@ public:
         int n = nums.size();
         if (n < 3) return result;
         sort(nums.begin(), nums.end());
-        for (int i = 0; i < n - 2; i++){
-            if (i > 0 && nums[i - 1] == nums[i])continue;
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i - 1] == nums[i]) continue;
             int j = i + 1;
             int k = n - 1;
             while (j < k) {
@@ -17,11 +17,11 @@ public:
                     result.push_back({nums[i], nums[j], nums[k]});
                     while (j < k && nums[j] == nums[j + 1]) j++;
                     j++;
-                    while (j < k && nums[k - 1] == nums[k]) k--;
+                    while (j < k && nums[k] == nums[k - 1])k--;
                     k--;
                 }
             }
         }
-        return result;   
+        return result;
     }
 };
