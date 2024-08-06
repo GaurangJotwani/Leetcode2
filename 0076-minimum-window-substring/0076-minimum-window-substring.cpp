68aa2217c4aad8c1dffb0baae26ff70335dcb873
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-06 10:38:32 -0400

    Time: 95 ms (9.22%), Space: 11.2 MB (17.22%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -22,11 +22,11 @@ public:
                     p.first = l;
                     p.second = r;
                 }
-                s_d[s[l]]--;
-                if (s_d[s[l]] == 0) {
-                    s_d.erase(s[l]);
-                }
-                l++;
+                // s_d[s[l]]--;
+                // if (s_d[s[l]] == 0) {
+                //     s_d.erase(s[l]);
+                // }
+                // l++;
                 while (l <= r && areMatching(s_d, t_d)) {
                         if (r - l + 1 < res) {
                             res = r - l + 1;
