9bbcfb8febb3e959023d1d9ecf1ba25ac5e675c7
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-13 16:11:52 -0700

    Time: 90 ms (80.91%), Space: 12.1 MB (35.98%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -10,24 +10,36 @@ public:
     }
     int minCut(string s) {
         int n = s.size();
-        vector<int> dp(n + 1, INT_MAX);
-        dp[n] = -1;
-        vector<vector<bool>> memo(n, vector<bool>(n, false));
+        vector<vector<bool>> isPalindrome(n, vector<bool>(n, false));
+        vector<int> dp(n, INT_MAX);
+        
+        // Build the isPalindrome table
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                memo[i][j] = isPalindrome(s, i, j);
+            isPalindrome[i][i] = true;  // Single characters are palindromes
+        }
+        for (int i = 0; i < n - 1; i++) {
+            isPalindrome[i][i + 1] = (s[i] == s[i + 1]);  // Two consecutive characters
+        }
+        for (int length = 3; length <= n; length++) {
+            for (int i = 0; i <= n - length; i++) {
+                int j = i + length - 1;
+                isPalindrome[i][j] = (s[i] == s[j]) && isPalindrome[i + 1][j - 1];
             }
         }
 
-        for (int i = n - 1; i >= 0; i--) {
-            int ans = INT_MAX;
-            for (int j = i; j < n; j++) {
-                if (memo[i][j]) {
-                    ans = min(ans, 1 + dp[j + 1]);
+        // Fill dp array
+        for (int j = 0; j < n; j++) {
+            if (isPalindrome[0][j]) {
+                dp[j] = 0;
+            } else {
+                for (int i = 1; i <= j; i++) {
+                    if (isPalindrome[i][j]) {
+                        dp[j] = min(dp[j], dp[i - 1] + 1);
+                    }
                 }
             }
-            dp[i] = ans;
         }
-        return dp[0];
+        
+        return dp[n - 1];
     }
 };
