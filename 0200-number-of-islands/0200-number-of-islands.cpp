f6e27ccebcab67d3c3a302be4479d24e8b550224
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-14 19:57:58 -0700

    Time: 95 ms (6.24%), Space: 31.2 MB (6.35%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
-    vector<vector<char>> grid;
-    map<pair<int, int>, bool> seen;
     int ROWS;
     int COLS;
-    vector<vector<int>>dir{{1, 0},{-1, 0},{0, 1},{0, -1}};
-    
+    vector<vector<int>> dir = {{1, 0},{-1, 0},{0, 1},{0, -1}};
+    vector<vector<char>> grid;
+
+    void dfs(int r, int c, vector<vector<bool>> &visited) {
+        for (auto d: dir) {
+            int row = r + d[0];
+            int col = c + d[1];
+            if (row >= 0 && row < ROWS && col >= 0 && col < COLS && !visited[row][col] && grid[row][col] == '1') {
+                visited[row][col] = true;
+                dfs(row, col, visited);
+            }
+        }
+    }
+
     int numIslands(vector<vector<char>>& grid) {
         this->grid = grid;
         ROWS = grid.size();
         COLS = grid[0].size();
+        vector<vector<bool>> visited(ROWS, vector<bool>(COLS, false));
         int res = 0;
         for (int r = 0; r < ROWS; r++) {
             for (int c = 0; c < COLS; c++) {
-                if (seen.find({r, c}) != seen.end() || grid[r][c] == '0') continue;
-                helper(r, c);
-                res++;
+                if (!visited[r][c] && grid[r][c] == '1') {
+                    visited[r][c]= true;
+                    res++;
+                    dfs(r, c, visited);
+                }
             }
         }
         return res;
-    }
-private:
-    void helper(int r, int c) {
-        seen[{r, c}] = true;
-        for (auto &d: dir) {
-            int row = r + d[0];
-            int col = c + d[1];
-            if (row >= 0 && row < ROWS && 
-                col >= 0 && col < COLS &&
-                grid[row][col] != '0' &&
-                seen.find({row, col}) == seen.end()) {
-                helper(row, col);
-            }
-        }
     }
 };
