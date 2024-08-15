6706ad1739ab5d01d5641cfbeddead38f07a6a32
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-15 08:33:42 -0700

    Time: 508 ms (5.82%), Space: 16.6 MB (62.12%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int target = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            target += nums[i];
+        int sum = 0;
+        for (auto num: nums) sum += num;
+        if (sum % 2 != 0 || nums.size() == 1) return false;
+        int n = sum / 2;
+
+        vector<vector<bool>> dp(nums.size(), vector<bool>(n + 1, false));
+        for (int i = 0; i < nums.size(); i++) dp[i][0] = true;
+        if (nums[0] <= n) {
+            dp[0][nums[0]] = true;
         }
         
-        if (target % 2 != 0) return false;
-        
-        
-        target /= 2;
-        
-        unordered_set<int> dp;
-        dp.insert(0);
-        
-        for (int i = 0; i < nums.size(); i++) {
-            unordered_set<int> dpNext;
-            for (auto it = dp.begin(); it != dp.end(); it++) {
-                if (*it + nums[i] == target) {
-                    return true;
+        for (int i = 1; i < nums.size(); i++) {
+            for (int j = 0; j <= n; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j - nums[i] >= 0) {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j - nums[i]];
                 }
-                dpNext.insert(*it + nums[i]);
-                dpNext.insert(*it);
             }
-            dp = dpNext;
         }
-        
-        
-        return false;
-        
+        return dp[nums.size() - 1][n];
+
     }
 };
