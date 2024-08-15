941a8337b3b7ab445b21a79bc3564fe86516413d
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-14 20:17:48 -0700

    Time: 7 ms (30.23%), Space: 16.7 MB (31.88%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -5,10 +5,15 @@ public:
         int n = cost.size();
         if (n == 1) return 0;
         vector<int> dp(n + 1);
+        int first = 0;
+        int second = 0;
+
         for (int i = 2; i < dp.size(); i++) {
-            dp[i] = min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
+            int res = min(first + cost[i - 2], second + cost[i - 1]);
+            first = second;
+            second = res;
         }
-        return dp[n];
+        return second;
 
     }
 };
