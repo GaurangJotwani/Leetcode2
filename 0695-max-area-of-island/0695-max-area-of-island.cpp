0c5068846187edfb6fc114477ea1790078d7a944
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-15 08:11:43 -0700

    Time: 45 ms (5.07%), Space: 35.8 MB (5.52%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -2,45 +2,35 @@ class Solution {
 public:
     int ROWS;
     int COLS;
-    map<pair<int, int>, bool> seen;
-    vector<vector<int>> directions = {{1,0},{-1,0},{0,1},{0,-1}};
     vector<vector<int>> grid;
+    vector<vector<int>> dir = {{1, 0},{-1, 0},{0, 1},{0, -1}};
     
+    int dfs(int r, int c, vector<vector<bool>> &visited) {
+        visited[r][c] = true;
+        int ans = 1;
+        for (auto d: dir) {
+            int row = r + d[0];
+            int col = c + d[1];
+            if (row >= 0 && row < ROWS && col >= 0 && col < COLS && !visited[row][col] && grid[row][col] == 1) {
+                ans += dfs(row, col, visited);
+            }
+        }
+        return ans;
+    }
+
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        
+        this->grid = grid;
         ROWS = grid.size();
         COLS = grid[0].size();
-        this->grid = grid;
-        
-        int maxArea = 0;
-        
+        vector<vector<bool>> visited(ROWS, vector<bool>(COLS, false));
+        int res = 0;
         for (int r = 0; r < ROWS; r++) {
             for (int c = 0; c < COLS; c++) {
-                if (grid[r][c] != 0 && seen.find({r, c}) == seen.end()) {
-                    seen[{r, c}] = true;
-                    maxArea = max(maxArea, dfs(r, c));
+                if (!visited[r][c] && grid[r][c] == 1) {
+                    res = max(res, dfs(r, c, visited));
                 }
             }
         }
-        
-        return maxArea;
-    }
-private:
-    int dfs(int r, int c) {
-        int res = 1;
-        for (auto &dir : directions) {
-            int row = r + dir[0];
-            int col = c + dir[1];
-            if (row >= 0 && row < ROWS 
-                && col >= 0 && col < COLS
-                && seen.find({row, col}) == seen.end() 
-                && grid[row][col] != 0
-               ) {
-                seen[{row, col}] = true;
-                res += dfs(row, col);
-            }
-        }
-        
         return res;
     }
 };
