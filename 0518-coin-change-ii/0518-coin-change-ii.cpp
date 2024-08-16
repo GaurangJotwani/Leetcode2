48764e0505313ee203eba688fa46c723404abaff
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-16 10:11:59 -0700

    Time: 17 ms (26.82%), Space: 20.3 MB (23.47%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,25 +2,26 @@ class Solution {
 public:
     int target;
     vector<int> coins;
+    vector<vector<int>> dp;
     
     int change(int amount, vector<int>& coins) {
         this->coins = coins;
         this->target = amount;
-        vector<vector<int>> dp(coins.size(), vector<int>(amount + 1, -1));
-        return helper(0, 0, dp);
+        dp = vector<vector<int>>(coins.size(), vector<int>(amount + 1, -1));
+        return helper(0, 0);
     }
 
-    int helper(int idx, int cSum, vector<vector<int>> &dp) {
+    int helper(int idx, int cSum) {
         if (cSum == target) return 1;
         if (idx == coins.size()) return 0;
         if (dp[idx][cSum] != -1) return dp[idx][cSum];
         int ans = 0;
         // include
         if (cSum + coins[idx] <= target) {
-            ans += helper(idx, cSum + coins[idx], dp);
+            ans += helper(idx, cSum + coins[idx]);
         }
         // exclude
-        ans += helper(idx + 1, cSum, dp);
+        ans += helper(idx + 1, cSum);
         return dp[idx][cSum] = ans;
     }
 };
