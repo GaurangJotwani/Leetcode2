24637812e4d69aa2d77bafb09c162c16b90c46ed
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-16 11:44:25 -0700

    Time: 61 ms (14.92%), Space: 31 MB (12.06%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -2,76 +2,41 @@ class Solution {
 public:
     int ROWS;
     int COLS;
-    vector<vector<int>> h;
     vector<vector<int>> dir = {{1, 0},{-1, 0},{0, 1},{0, -1}};
-    
+    vector<vector<int>> grid;
+
+    void dfs(int r, int c, vector<vector<bool>> &visited) {
+        visited[r][c] = true;
+        for (auto d: dir) {
+            int row = r + d[0];
+            int col = c + d[1];
+            if (row >= 0 and row < ROWS && col >= 0 && col < COLS && !visited[row][col] && grid[r][c] <= grid[row][col]) {
+                dfs(row, col, visited);
+            }
+        }
+    }
+
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         ROWS = heights.size();
         COLS = heights[0].size();
-        h = heights;
-        
-        map<pair<int, int>, bool> pac;
-        map<pair<int, int>, bool> atl;
-        
+        this->grid = heights;
+        vector<vector<bool>> pac(ROWS, vector<bool>(COLS, false));
+        vector<vector<bool>> atl(ROWS, vector<bool>(COLS, false));
+
+        for (int c = 0; c < COLS; c++) {
+            if(!pac[0][c]) dfs(0,c,pac);
+            if(!atl[ROWS - 1][c]) dfs(ROWS - 1,c,atl);
+        }
         for (int r = 0; r < ROWS; r++) {
-            for (int c = 0; c < COLS; c++) {
-                if (r == 0 || c == 0) {
-                    if (pac.find({r, c}) == pac.end()) {
-                        pac[{r, c}] = true;
-                        dfs(r, c, pac);
-                    }
-                } 
-                
-                if (r == ROWS - 1 || c == COLS - 1) {
-                    if (atl.find({r, c}) == atl.end()) {
-                        atl[{r, c}] = true;
-                        dfs(r, c, atl);
-                    }
-                }
-            }
+            if(!pac[r][0]) dfs(r,0,pac);
+            if(!atl[r][COLS-1]) dfs(r,COLS-1,atl);
         }
         vector<vector<int>> res;
         for (int r = 0; r < ROWS; r++) {
             for (int c = 0; c < COLS; c++) {
-                if (atl.find({r, c}) != atl.end() && pac.find({r, c}) != pac.end()) {
-                    res.push_back({r, c});
-                }
+                if (atl[r][c] && pac[r][c]) res.push_back({r,c});
             }
         }
         return res;
     }
-    
-private:
-    void dfs(int r, int c, map<pair<int, int>, bool>& vis) {
-        for (auto d: dir) {
-            int row = r + d[0];
-            int col = c + d[1];
-            if (row >= 0 && row < ROWS &&
-                col >= 0 && col < COLS &&
-                vis.find({row, col}) == vis.end() && 
-                h[row][col] >= h[r][c]
-               ) {
-                vis[{row, col}] = true;
-                dfs(row, col, vis);
-            }
-        }
-    }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
