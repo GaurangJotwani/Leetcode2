f1971699a4cfc7cfeffc15a06322b400386cd9c1
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-16 08:29:16 -0700

    Time: 15 ms (22.98%), Space: 15.8 MB (13.59%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
-    map<pair<int,int>, bool> visited;
-    int ROWS;
-    int COLS;
-    vector<vector<int>> dir = {{1, 0},{-1, 0},{0, 1},{0, -1}};
-    
+    void dfs(int r, int c, vector<vector<char>>& board, vector<vector<bool>> &visited) {
+        visited[r][c] = true;
+        board[r][c] = 'S';
+        for (auto d: dir) {
+            int row = r + d[0];
+            int col = c + d[1];
+            if (row >= 0 && row < ROWS && col >= 0 && col < COLS && !visited[row][col] && board[row][col] == 'O') {
+                dfs(row, col, board, visited);
+            }
+        }
+    }
+
     void solve(vector<vector<char>>& board) {
         ROWS = board.size();
         COLS = board[0].size();
-        for (int r = 0; r < ROWS; r++) {
+        vector<vector<bool>> visited(ROWS, vector<bool>(COLS, false));
+        for (int r = 0; r< ROWS; r++) {
             for (int c = 0; c < COLS; c++) {
-                if (r == 0 || c == 0 || r == ROWS - 1 || c == COLS - 1) {
-                    if (board[r][c] == 'O' && visited.find({r, c}) == visited.end()) {
-                        visited[{r, c}] = true;
-                        board[r][c] = 'S';
-                        dfs(r, c, board);
+                if (r == 0 || r == ROWS - 1 || c == 0 || c == COLS - 1) {
+                    if (!visited[r][c] && board[r][c] == 'O') {
+                        dfs(r, c, board, visited);
                     }
                 }
             }
         }
-        for (int r = 0; r < ROWS; r++) {
+        for (int r = 0; r< ROWS; r++) {
             for (int c = 0; c < COLS; c++) {
                 if (board[r][c] == 'S') {
                     board[r][c] = 'O';
@@ -28,22 +34,10 @@ public:
                 }
             }
         }
-        
+
     }
 private:
-    void dfs(int r, int c, vector<vector<char>>& board) {
-        for (auto d: dir) {
-            int row = r + d[0];
-            int col = c + d[1];
-            if (row >= 0 && row < ROWS &&
-                col >= 0 && col < COLS &&
-                visited.find({row, col}) == visited.end() &&
-                board[row][col] == 'O'
-               ) {
-                board[row][col] = 'S';
-                visited[{row, col}] = true;
-                dfs(row, col, board);
-            }
-        }
-    }
+    int ROWS;
+    int COLS;
+    vector<vector<int>> dir = {{0, 1},{0, -1},{1, 0},{-1, 0}};
 };
