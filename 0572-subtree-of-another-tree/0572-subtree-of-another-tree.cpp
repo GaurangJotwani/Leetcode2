d8fc7619915d0f076b5c969f351fce7083e3b008
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-16 11:09:03 -0700

    Time: 11 ms (93.39%), Space: 28.2 MB (11.67%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -12,16 +12,15 @@
 class Solution {
 public:
     bool isSubHelper(TreeNode* root, TreeNode* subRoot) {
-        if (subRoot == NULL and root == NULL) return true;
+        if (subRoot == NULL && root == NULL) return true;
         if (subRoot == NULL || root == NULL) return false;
-        return root->val == subRoot->val and isSubHelper(root->left, subRoot->left) and isSubHelper(root->right, subRoot->right);
+        return root->val == subRoot->val &&
+               isSubHelper(root->left, subRoot->left) &&
+               isSubHelper(root->right, subRoot->right);
     }
-
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
         if (root == NULL) return false;
-        // Check if current root matches subRoot
         if (isSubHelper(root, subRoot)) return true;
-        // Otherwise, continue searching in the left and right subtrees
         return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
     }
 };
