e6f24d12826e723120133249d9ad963d3ef4b1b4
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-16 09:19:05 -0700

    Time: 0 ms (100%), Space: 9.4 MB (64.44%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
+vector<int> nums;
+int n;
     int rob(vector<int>& nums) {
-        
-        int n = nums.size();
-        
-        if (n == 1) return nums[0];
-        
-        int range1 = robber(nums, 0, n - 2);
-        int range2 = robber(nums, 1, n - 1);
-        
-        return max(range1, range2);
-        
-        
+        this->nums = nums;
+        n = nums.size();
+        if (nums.size() == 1) return nums[0];
+        if (nums.size() == 2) return max(nums[0], nums[1]);
+
+        int f = helper(0, nums.size() - 2);
+        int s = helper(1, nums.size() - 1);
+        return max(f, s);   
     }
 private:
-    int robber(vector<int>& nums, int start, int end) {
-        int prev = 0;
-        int curr = 0;
-        int next = 0;
-        
-        for (int i = start; i <= end; i++) {
-            next = max(prev + nums[i], curr);
-            prev = curr;
-            curr = next;
-            
+    int helper(int start, int end) {
+        int first = 0;
+        int second = nums[start];
+        for (int i = start + 1; i <= end; i++) {
+            int temp_max = max(second, first + nums[i]);
+            first = second;
+            second = temp_max;
         }
-        
-        return curr;
+        return second;
     }
 };
