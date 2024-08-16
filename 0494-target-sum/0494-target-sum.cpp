a8609d742a122463b1e704bbbeed03da4787820b
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-16 08:02:43 -0700

    Time: 270 ms (30.18%), Space: 62.1 MB (9.39%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
+    vector<int> nums;
+    int target;
+    map<pair<int, int>, int> dp;
     int findTargetSumWays(vector<int>& nums, int target) {
-        return backtrack(nums, target, 0, 0);
+        this->nums = nums;
+        this->target = target;
+        return helper(0,0);
     }
 private:
-    map<pair<int, int>, int> dp;
-    
-    int backtrack(vector<int>& nums, int target, int i, int total) {
-        if (i == nums.size()){
-            return total == target ? 1 : 0;
-        }
-        
-        if (dp.find({i, total}) != dp.end()) {
-            return dp[{i, total}];
-        }
-        
-        dp[{i, total}] = backtrack(nums, target, i + 1, total + nums[i]) +  backtrack(nums, target, i + 1, total - nums[i]);
-        
-        return dp[{i, total}];
+    int helper(int idx, int cSum) {
+        if (idx == nums.size()) return cSum == target ? 1 : 0;
+        if (dp.find({idx, cSum}) != dp.end()) return dp[{idx, cSum}];
+        int ans = 0;
+        // either add it    
+        ans += helper(idx + 1, cSum + nums[idx]);
+        // either subtract it
+        ans += helper(idx + 1, cSum - nums[idx]);
+        dp[{idx, cSum}] = ans;
+        return ans;
     }
 };
