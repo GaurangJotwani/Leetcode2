939e9e36b338b857ff5a1281a1d029d28e471da7
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-16 10:35:29 -0700

    Time: 4 ms (57.32%), Space: 14.9 MB (14.11%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int size = prices.size();
-        int index = 0;
-        int canBuy = 1;
-        vector<vector<int>> dp(size + 1, vector<int>(2, -1));
-        
-        return helper(index, canBuy, dp, prices);
-    }
-private:
-    int helper(int i, int canBuy, vector<vector<int>> &dp, vector<int>& prices) {
-        
-        if (i >= prices.size()) {
-            return 0;
-        }
-        
-        if (dp[i][canBuy] != -1) {
-            return dp[i][canBuy];
-        }
-        int val1;
-        if (canBuy == 1) {
-            val1 = -1 * prices[i] + helper(i + 1, 0, dp, prices);
+    vector<int> prices;
+    vector<vector<int>> dp;
+    int helper(int idx, bool buy) {
+        if (idx >= prices.size()) return 0;
+        if (dp[idx][buy] != -1) return dp[idx][buy]; 
+        int ans = 0;
+        // buy
+        if (buy) {
+            ans = max(ans, -1 * prices[idx] + helper(idx + 1, !buy));
         } else {
-            val1 = prices[i] + helper(i + 2, 1, dp, prices);
+            //sell
+            ans = max(ans, prices[idx] + helper(idx + 2, !buy));
         }
-        
-        int val2 = helper(i + 1, canBuy, dp, prices);
-        
-        dp[i][canBuy] = max(val1, val2);
-        return dp[i][canBuy];
+        // do nothing
+        ans = max(ans, helper(idx + 1, buy));
+        return dp[idx][buy] = ans;
+    }
+
+    int maxProfit(vector<int>& prices) {
+        this->prices = prices;
+        dp = vector<vector<int>>(prices.size(), vector<int>(2, -1));
+        return helper(0, true);
     }
 };
