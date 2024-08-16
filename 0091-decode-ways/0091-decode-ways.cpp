200ad2afbfb0f10726f385947efbbecd5da94d32
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-15 19:49:01 -0700

    Time: 4 ms (33.08%), Space: 7.9 MB (79.21%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -7,16 +7,16 @@ public:
         vector<int> dp(n + 1, 0);
         dp[0] = 1;
         dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            int dig = s[i - 1] - '0';
+        for (int i = 1; i < n; i++) {
+            int dig = s[i] - '0';
             if (dig != 0) {
-                dp[i] = dp[i - 1];
+                dp[i + 1] = dp[i];
             }
 
-            int prev_dig = s[i - 2] - '0';
+            int prev_dig = s[i - 1] - '0';
             cout << prev_dig << endl;
             if (prev_dig == 1 or (prev_dig == 2 and dig <= 6)) {
-                dp[i] += dp[i - 2];
+                dp[i + 1] += dp[i - 1];
             }
         }
         return dp[n];
