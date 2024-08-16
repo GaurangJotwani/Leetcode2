0ed450c92c9dd07d52af1c9b49d7ca3242f768e4
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-15 20:24:16 -0700

    Time: 3 ms (84.64%), Space: 16.7 MB (32.79%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-       if (nums.size() == 0) return 0;
+        double maxi = INT_MIN;
+        double prod=1;
 
-        long long max_so_far = nums[0];
-        long long min_so_far = nums[0];
-        long long result = max_so_far;
-
-        for (int i = 1; i < nums.size(); i++) {
-            long long curr = nums[i];
-
-            // Calculate the potential maximum and minimum products
-            long long temp_max =
-                max(curr, max(max_so_far * curr, min_so_far * curr));
-            min_so_far = min(curr, min(max_so_far * curr, min_so_far * curr));
-
-            // Update max_so_far after min_so_far to avoid overwriting it
-            max_so_far = temp_max;
-            if (min_so_far < 0 && curr > (INT_MIN + 1) / min_so_far) {
-                max_so_far = 1;
-                min_so_far = 1;
-            }
-            // Update the result with the maximum product found so far
-            result = max(max_so_far, result);
+        for(int i=0;i<nums.size();i++)
+        {
+          prod*=(double)nums[i];
+          maxi=max(prod,maxi);
+          if(prod==0)
+           prod=1;
         }
+        prod=1;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+          prod*=(double)nums[i];
 
-        return result;
+          maxi=max(prod,maxi);
+          if(prod==0)
+           prod=1;
+        }
+        return maxi;
     }
 };
