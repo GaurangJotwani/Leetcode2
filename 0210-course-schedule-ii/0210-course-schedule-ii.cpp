3702ee9431f4baacbe427343a9a3bedaae82da9c
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-17 14:25:48 -0700

    Time: 11 ms (91.07%), Space: 17.8 MB (36.67%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-
-        vector<int> indegrees(numCourses, 0);
-        vector<vector<int>> adjList(numCourses);
         vector<int> res;
+        vector<int> incoming(numCourses, 0);
+        vector<vector<int>> adjList(numCourses);
+        queue<int> q;
 
         for (auto pre: prerequisites) {
-            indegrees[pre[0]]++;
             adjList[pre[1]].push_back(pre[0]);
+            incoming[pre[0]]++;
         }
 
-        for (auto &l: adjList) {
-            sort(l.begin(), l.end());
-        }
-
-
-        queue<int> q;
         for (int i = 0; i < numCourses; i++) {
-            if (indegrees[i] == 0) q.push(i);
+            if (incoming[i] == 0) q.push(i);
         }
 
-        while(!q.empty()) {
+        while (!q.empty()) {
             int node = q.front();
             q.pop();
             res.push_back(node);
             for (auto nei: adjList[node]) {
-                indegrees[nei]--;
-                if (indegrees[nei] == 0) q.push(nei);
+                incoming[nei]--;
+                if (incoming[nei] == 0) q.push(nei);
             }
         }
-
-        if (res.size() != numCourses) return vector<int>();
-
-        return res;
-        
+        if (res.size() == numCourses) return res;
+        return {};
     }
 };
