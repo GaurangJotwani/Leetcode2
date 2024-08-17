5ccc91bb7e3e79ddc67dddd7ee63e991778b73d2
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-16 20:40:40 -0700

    Time: 19 ms (35.82%), Space: 18.3 MB (24.18%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    unordered_map<int, bool> visited;
-    unordered_map<int, bool> visiting;
-    unordered_map<int, vector<int>> adj_list;
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        for (auto v: prerequisites) {
-            adj_list[v[0]].push_back(v[1]);
-        }
-        for (int i = 0; i < numCourses; i++) {
-            if (visited.find(i) == visited.end()) {
-                if (!dfs(i)) return false;
+    vector<int> res;
+    vector<vector<int>> adjList;
+    bool dfs(int node, vector<int> &visit) {
+        visit[node] = 1;
+        for (auto nei: adjList[node]) {
+            if (visit[nei] == 1) return false;
+            else if (visit[nei] == 0) {
+                if (!dfs(nei, visit)) return false;
             }
         }
+        visit[node] = 2;
+        res.push_back(node);
         return true;
     }
-private:
-    bool dfs(int node) {
-        if (visiting.find(node) != visiting.end()) return false;
-        visiting[node] = true;
-        
-        for (auto i: adj_list[node]) {
-            if (visited.find(i) == visited.end()) {
-                if (!dfs(i)) return false;
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        adjList = vector<vector<int>>(numCourses);
+        vector<int> visit(numCourses, 0);
+        for (auto pre: prerequisites) {
+            adjList[pre[1]].push_back(pre[0]);
+        }
+        for (int i = 0; i < numCourses; i++) {
+            if (visit[i] == 0) {
+                if(!dfs(i, visit)) return false;
             }
         }
-        
-        visiting[node] = false;
-        visited[node] = true;
-        
         return true;
     }
 };
