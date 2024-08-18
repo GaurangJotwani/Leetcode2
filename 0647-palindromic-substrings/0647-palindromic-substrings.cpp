4d36e3db76736d3249f9440a2d063689a7560e26
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-17 17:16:00 -0700

    Time: 46 ms (24.91%), Space: 11.5 MB (55.1%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -3,24 +3,19 @@ public:
     int countSubstrings(string s) {
         int n = s.size();
         vector<vector<bool>> dp(n, vector<bool>(n, 0));
+        int res = 0;
 
         for (int l = 1; l <= n; l++) {
             int j = l - 1;
             for (int i = 0; i < n - l + 1; i++) {
                 if (l == 1) dp[i][j] = true;
                 else if (l == 2) dp[i][j] = s[i] == s[j];
-                else {
-                    dp[i][j] = s[i] == s[j] and dp[i + 1][j - 1]; 
-                }
+                else dp[i][j] = s[i] == s[j] and dp[i + 1][j - 1];
+                res += dp[i][j];
                 j++;
             }
         }
-        int res = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if (dp[i][j]) res++;
-            }
-        }
+
         return res;
     }
 };
