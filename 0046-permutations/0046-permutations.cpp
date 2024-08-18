f5bfa8b35888a19e975719abb56e9b9134d14ba0
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-17 18:03:45 -0700

    Time: 0 ms (100%), Space: 11 MB (10.37%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> result;
-        vector<int> curr;
-        helper(curr, nums, result);
-        return result;
+    vector<vector<int>> res;
+    vector<int> nums;
+    void permute(vector<int> &curr, vector<int> remain) {
+        if (curr.size() == nums.size()) {
+            res.push_back(curr);
+            return;
+        }
+        for (int i = 0; i < remain.size(); i++) {
+            curr.push_back(remain[i]);
+            vector<int> temp;
+            for (int j = 0; j < i; j++) temp.push_back(remain[j]);
+            for (int j = i + 1; j < remain.size(); j++) temp.push_back(remain[j]);
+            permute(curr, temp);
+            curr.pop_back();
+        }
     }
-
-private:
-    void helper(vector<int>& curr, vector<int>& nums, vector<vector<int>>& result) {
-        if (nums.size() == 0) {
-            result.push_back(curr);
-        }
-
-        for (int i = 0; i < nums.size(); i++) {
-            curr.push_back(nums[i]);  // Add current number to current permutation
-
-            // Remove the number from nums and call helper recursively
-            int num = nums[i];
-            nums.erase(nums.begin() + i);
-            helper(curr, nums, result);
-
-            // Restore the number back to nums
-            nums.insert(nums.begin() + i, num);
-            curr.pop_back();  // Remove last number added to current permutation
-        }
+    vector<vector<int>> permute(vector<int>& nums) {
+        this->nums = nums;
+        vector<int> curr;
+        permute(curr, nums);
+        return res;
     }
 };
