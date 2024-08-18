5b4cc8b193a962ea3e8bda1a0f700585ae032003
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-17 20:37:51 -0700

    Time: 0 ms (100%), Space: 8.5 MB (34.42%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
-    vector<string> letterCombinations(string digits) {
-         
-        unordered_map<char, string>mp = {
-            {'2', "abc"},
-            {'3', "def"},
-            {'4', "ghi"},
-            {'5', "jkl"},
-            {'6', "mno"},
-            {'7', "pqrs"},
-            {'8', "tuv"},
-            {'9', "wxyz"}
-        };
-        vector<string> result;
-        if (digits.empty()) return result;
-        string curr = "";
-        helper(0, curr, digits, mp, result);
-        return result;
-    }
-private:
-    void helper(int idx, string curr, string& digits, unordered_map<char, string>& mp, vector<string>& result) {
+    vector<string> res;
+    string digits;
+    vector<string> mp = {"#", "#", "abc", "def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+    void dfs(int idx, string curr) {
         if (idx == digits.size()) {
-            result.push_back(curr);
+            res.push_back(curr);
             return;
         }
-        
-        for (auto c: mp[digits[idx]]) {
-            string s = curr + c;
-            helper(idx + 1, s, digits, mp, result);
+        int dig = digits[idx] - '0';
+        for (auto c: mp[dig]) {
+            curr.push_back(c);
+            dfs(idx + 1, curr);
+            curr.pop_back();
         }
-        
+    }
+
+    vector<string> letterCombinations(string digits) {
+        if (digits.size() == 0) return {};
+        this->digits = digits;
+        dfs(0, "");
+        return res;
     }
 };
