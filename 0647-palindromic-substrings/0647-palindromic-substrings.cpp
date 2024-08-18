a18218e30c04f6a5a8bfe980d9169961c870d971
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-17 17:13:55 -0700

    Time: 42 ms (27.42%), Space: 11.6 MB (50.83%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     int countSubstrings(string s) {
+        int n = s.size();
+        vector<vector<bool>> dp(n, vector<bool>(n, 0));
+
+        for (int l = 1; l <= n; l++) {
+            int j = l - 1;
+            for (int i = 0; i < n - l + 1; i++) {
+                if (l == 1) dp[i][j] = true;
+                else if (l == 2) dp[i][j] = s[i] == s[j];
+                else {
+                    dp[i][j] = s[i] == s[j] and dp[i + 1][j - 1]; 
+                }
+                j++;
+            }
+        }
         int res = 0;
-        for (int i = 0; i < s.size(); i++) {
-           getMaxPalindrome(s, i, i, res);
-           getMaxPalindrome(s, i, i + 1, res);
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (dp[i][j]) res++;
+            }
         }
         return res;
-    }
-
-private:
-    void getMaxPalindrome(string s, int start, int end, int& result) {
-        
-        while (start >= 0 && end < s.size() && s[start] == s[end]) {
-            start -= 1;
-            end += 1;
-            result++;
-        }
     }
 };
