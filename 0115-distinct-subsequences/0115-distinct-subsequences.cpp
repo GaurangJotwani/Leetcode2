790d20cdc63cd6c6f3c9a38a78016bc4c0ec30c0
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-17 21:11:08 -0700

    Time: 18 ms (62.06%), Space: 12.3 MB (82.28%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -6,15 +6,12 @@ public:
     int solve(int i, int j) {
         if (i == s.size() && j == t.size()) return 1;
         if (i == s.size()) return 0;
-        if (j == t.size()) return 1;
+        
         if (dp[i][j] != -1) return dp[i][j];
-
         int ans = 0;
-
         if (s[i] == t[j]) ans += solve(i + 1, j + 1);
         ans += solve(i + 1, j);
         return dp[i][j] = ans;
-
     }
 
     int numDistinct(string s, string t) {
