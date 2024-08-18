691f373ce700ad7edd9b30f918a5921d62dedde5
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-17 18:32:41 -0700

    Time: 0 ms (100%), Space: 13.2 MB (29.58%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> result;
-        vector<int> curr;
+        this->target = target;
         sort(candidates.begin(), candidates.end());
-        helper(0, curr, 0, result, candidates, target);
-        return result;
+        this->nums = candidates;
+        vector<int> curr;
+        knapsack(0, 0, curr);
+        return res;
     }
 private:
-    void helper(int idx, vector<int>& curr, int cSum, vector<vector<int>>& result, vector<int>& candidates, int target) {
-        
-        if (cSum == target) {
-            result.push_back(curr);
-            return;
-        }
-        
-        if (idx == candidates.size()) return;
-        if (cSum > target) return;
-        
-        curr.push_back(candidates[idx]);
-        helper(idx + 1, curr, cSum + candidates[idx], result, candidates, target);
-        curr.pop_back();
-        idx += 1;
-        while (idx < candidates.size() && candidates[idx - 1] == candidates[idx]) {
-            idx++;
-        }
-        helper(idx, curr, cSum, result, candidates, target);
+int target;
+vector<int> nums;
+vector<vector<int>> res;
+void knapsack(int idx, int cSum, vector<int> &curr) {
+    if (cSum == target) {
+        res.push_back(curr);
+        return;
     }
+    if (idx == nums.size()) return;
+
+    // include in answer
+    if (cSum + nums[idx] <= target) {
+        curr.push_back(nums[idx]);
+        knapsack(idx + 1, cSum + nums[idx], curr);
+        curr.pop_back();
+    }
+    // exclude in answer
+    idx++;
+    while (idx < nums.size() && nums[idx] == nums[idx - 1])idx++;
+    knapsack(idx, cSum, curr);
+}
+
 };
