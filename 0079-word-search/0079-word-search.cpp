f83dd9fb8c970f586cca276c386b0660929245f7
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-17 20:21:20 -0700

    Time: 513 ms (59.89%), Space: 12 MB (22.74%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -5,8 +5,8 @@ public:
     
     
     bool exist(vector<vector<char>>& board, string word) {
-    for (unsigned int i = 0; i < board.size(); i++) 
-        for (unsigned int j = 0; j < board[0].size(); j++) 
+    for (int i = 0; i < board.size(); i++) 
+        for (int j = 0; j < board[0].size(); j++) 
             if (dfs(board, i, j, word))
                 return true;
     return false;
