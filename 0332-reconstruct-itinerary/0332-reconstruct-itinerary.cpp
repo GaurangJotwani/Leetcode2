bc1a2158c5138b450bb3ecde0d848462659f5303
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-18 10:59:43 -0700

    Time: 21 ms (31.08%), Space: 19.5 MB (45%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-
-    int FLIGHT_COUNT;
-    unordered_map<string, vector<string>> adjList;
-    unordered_map<string, vector<bool>> visited;
-    vector<string> result;
-
-    void dfs(string node) {
-        for (int i = 0; i < adjList[node].size(); i++) {
-            string nei = adjList[node][i];
-            if (!visited[node][i]) {
-                visited[node][i] = true;
-                dfs(nei);
-            }
+    vector<string> res;
+    
+    void postOrderDfs(string node, unordered_map<string, vector<string>> &adjList, unordered_map<string, int> &outgoing) {
+        cout << node << " " << outgoing[node] << endl;
+        while (outgoing[node] > 0) {
+            outgoing[node]--;
+            postOrderDfs(adjList[node][outgoing[node]], adjList, outgoing);
         }
-
-        result.push_back(node);
+        res.push_back(node);
     }
-
     vector<string> findItinerary(vector<vector<string>>& tickets) {
-        FLIGHT_COUNT = tickets.size();
-
-        for (auto &ticket: tickets) {
+        unordered_map<string, vector<string>> adjList;
+        unordered_map<string, int> outgoing;
+        for(auto ticket: tickets) {
             adjList[ticket[0]].push_back(ticket[1]);
-        }
-
+            outgoing[ticket[0]]++;
+        };
         for (auto &l: adjList) {
             sort(l.second.begin(), l.second.end());
-            visited[l.first] = vector<bool>(l.second.size(), false);
-        }
+            reverse(l.second.begin(), l.second.end());
+        };
+        
+        postOrderDfs("JFK", adjList, outgoing);
 
-        dfs("JFK");
-        reverse(result.begin(), result.end());
-        return result;
+        reverse(res.begin(), res.end());
+        return res;
+
     }
 };
