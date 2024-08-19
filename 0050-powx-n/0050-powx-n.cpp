47566903bcdc48506e99c44a3456f0d7e2fe0358
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-19 12:57:43 -0700

    Time: 3 ms (39.42%), Space: 8.1 MB (78.97%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double solve(double x, int n) {
+        
+        double res = 1;
+        while (n != 0) {
+            if (n & 1) res = res * x;
+            x = x * x;
+            n = n / 2;
+        }
+        return res;
+    }
+
+    double myPow(double x, int n) {
+        if (n == 0) return 1;
+        double ans = solve(x, abs(n));
+        if (n > 0) return ans;
+        else return 1/ans;
+    }
+};
