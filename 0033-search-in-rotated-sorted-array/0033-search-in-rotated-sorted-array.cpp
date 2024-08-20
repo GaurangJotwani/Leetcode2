2127e14a86a0fbded7dd033b8cb8ca25a83f127f
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-20 12:53:21 -0700

    Time: 8 ms (9.83%), Space: 13.9 MB (28.88%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,31 +1,45 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        
-        int left = 0;
-        int right = nums.size() - 1;
-        
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] == target) {
-                return mid;
+        // find pivot index
+        int l = 0;
+        int r = nums.size() - 1;
+        int pivIdx = -1;
+        while (l <= r) {
+            if (nums[l] <= nums[r]) {
+                pivIdx = l; 
+                break;
             }
-            
-            if (nums[left] <= nums[mid]) {
-              if (target >= nums[left] and target <= nums[mid]) {
-                  right = mid - 1;
-              } else {
-                  left = mid + 1;
-              }
-            } else {
-                if (target >= nums[mid] && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            } 
+            int mid = l + ((r - l) / 2);
+            if (nums[mid] > nums[mid + 1]) {
+                pivIdx = mid + 1;
+                break;
+            }
+            if (nums[mid] < nums[mid - 1]) {
+                pivIdx = mid;
+                break;
+            }
+            if (nums[mid] >= nums[l]) l = mid + 1;
+            else r = mid - 1;
         }
-        
+        cout << pivIdx;
+        if (target >= nums[pivIdx] && target <= nums[nums.size() - 1]) {
+            l = pivIdx;
+            r = nums.size() - 1;
+        } else if (target >= nums[0]) {
+            l = 0;
+            r = pivIdx - 1;
+        } else return -1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < target) l = mid + 1;
+            else r = mid - 1;
+        }
+
+
         return -1;
+
     }
 };
