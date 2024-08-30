72f9692fa373b0184f940d1352949af72b2928c5
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-29 21:49:30 -0700

    Time: 66 ms (73%), Space: 50.1 MB (5.25%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cs
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cs
@@ -2,17 +2,18 @@ public class Solution {
     public string LongestCommonPrefix(string[] strs) {
         var res = new StringBuilder("");
         int minLength = int.MaxValue;
-        foreach(var word in strs) minLength = Math.Min(word.Length, minLength);
-        if (minLength == 0) return "";
         if (strs.Length == 1) return strs[0];
 
-        for (int j = 1; j <= minLength; j++) {
+        int j = 1;
+        while (true) {
             for(int i = 1; i < strs.Length; i++) {
+                if (strs[i - 1].Length == 0 ||  strs[i].Length == 0) return "";
+                if (strs[i].Length == j - 1 || strs[i - 1].Length == j - 1) return res.ToString();
                 if (strs[i].Substring(0, j) != strs[i - 1].Substring(0, j)) return res.ToString();
             }
             res.Append(strs[0][j - 1]);
+            j++;
         }
-
         return res.ToString();
     }
 }
