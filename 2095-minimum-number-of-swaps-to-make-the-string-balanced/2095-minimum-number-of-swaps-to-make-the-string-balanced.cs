a555840a969536c25989f40c9c1acb31b99bfa97
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-31 17:46:33 -0700

    Time: 72 ms (24.44%), Space: 56.7 MB (75.56%) - LeetHub

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int MinSwaps(string s) {
+        int res = 0;
+        int cSum = 0;
+        foreach (var c in s) {
+            if (c == ']') cSum--;
+            else cSum++;
+            if (cSum < 0) res = Math.Min(cSum, res);
+        }
+        if (res == 0) return 0;
+        double tmp = ((double)Math.Abs(res)) / 2;
+
+        return (int)Math.Ceiling(tmp);
+    }
+}
