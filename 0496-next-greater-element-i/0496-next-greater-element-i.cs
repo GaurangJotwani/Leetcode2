73bb07c8a25c77fe16a2b115d973ebfb0fae1406
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-08-31 17:25:19 -0700

    Time: 117 ms (10.96%), Space: 46.8 MB (58.57%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int[] NextGreaterElement(int[] nums1, int[] nums2) {
+        var stk = new Stack<int>();
+        var mp = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums2.Length; i++) {
+            var num = nums2[i];
+            while (stk.Count != 0 && stk.Peek() < num) {
+                var top = stk.Peek();
+                mp[top] = num;
+                stk.Pop();
+            }
+            stk.Push(num);
+        }
+        int[] res = new int[nums1.Length];
+        for(int i = 0; i < nums1.Length; i++) {
+            var num = nums1[i];
+            if (mp.ContainsKey(num)) {
+                res[i] = mp[num];
+            } else res[i] = -1;
+        }
+        return res;
+    }
+}
