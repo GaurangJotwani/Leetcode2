a3d71d34e98d212438ac9e1371a8b18376fbb77b
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-09-02 14:02:19 -0700

    Time: 376 ms (81.4%), Space: 66.5 MB (16.28%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cs
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cs
@@ -1,12 +1,10 @@
 public class Solution {
     public long ZeroFilledSubarray(int[] nums) {
         long res = 0;
-        var lstIndex = new Dictionary<int, int>(); //{index, array_size}
         for (int i = 0; i < nums.Length; i++) {
             long increment = 1;
             while (i < nums.Length && nums[i] == 0) {
-                res += increment;
-                increment++;
+                res += increment++;
                 i++;
             }
         }
