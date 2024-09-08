d859fb8feb409619059367d3fb45f8ac9ad87aa7
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-09-07 23:08:59 -0700

    Time: 240 ms (14.68%), Space: 128.9 MB (96.74%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public void ReverseString(char[] s) {
+        
+        int l = 0;
+        int r = s.Length - 1;
+        while (l < r) {
+            var tmp = s[l];
+            s[l] = s[r];
+            s[r] = tmp;
+            l++; r--;
+        }
+    }
+}
