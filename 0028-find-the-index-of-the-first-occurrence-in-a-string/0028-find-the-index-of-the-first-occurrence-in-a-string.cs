f76a7eeb5fe06600816d700ffc56c7a53a9cb20b
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-09-07 22:39:08 -0700

    Time: 34 ms (97.66%), Space: 37.2 MB (98.76%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int StrStr(string haystack, string needle) {
+        if (needle.Length > haystack.Length) return -1;
+
+        for (int i = 0; i <= haystack.Length - needle.Length; i++) {
+            bool found = true;
+            for (int j = 0; j < needle.Length; j++) {
+                if (haystack[i + j] != needle[j]) {
+                    found = false;
+                    break;
+                }
+            }
+            if (found) return i;
+        }
+
+        return -1;
+    }
+}
