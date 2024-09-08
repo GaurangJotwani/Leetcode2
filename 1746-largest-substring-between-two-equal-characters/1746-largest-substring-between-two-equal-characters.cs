c6745d1bda42f1e743b6b2bda00cebbc7cd2dca8
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-09-07 21:03:18 -0700

    Time: 43 ms (75.42%), Space: 37.1 MB (100%) - LeetHub

--- 1746-largest-substring-between-two-equal-characters/1746-largest-substring-between-two-equal-characters.cs
+++ 1746-largest-substring-between-two-equal-characters/1746-largest-substring-between-two-equal-characters.cs
@@ -2,15 +2,12 @@ public class Solution {
     public int MaxLengthBetweenEqualCharacters(string s) {
 
         var firstSeen = new int[26];
-        for (int i = 0; i < 26; i++) firstSeen[i] = -1;
+        for (var i = 0; i < 26; i++) firstSeen[i] = -1;
         int res = -1, n = s.Length;
 
-        for (int i = 0; i < n; i++) {
-            if(firstSeen[s[i] - 'a'] != -1) {
-                res = Math.Max(i - firstSeen[s[i] - 'a'] - 1, res);
-            } else {
-                firstSeen[s[i] - 'a'] = i;
-            }
+        for (var i = 0; i < n; i++) {
+            if(firstSeen[s[i] - 'a'] != -1) res = Math.Max(i - firstSeen[s[i] - 'a'] - 1, res);
+            else firstSeen[s[i] - 'a'] = i;
         }
 
         return res;
