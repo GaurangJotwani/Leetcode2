f2244c38768932c8f2717e31b86778ed467c9f19
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-09-08 23:57:10 -0700

    Time: 61 ms (17.88%), Space: 39.1 MB (30.56%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public string DecodeString(string s) {
+        var stk = new Stack<string>();
+
+        foreach (var c in s) {
+            if (c != ']') {
+                stk.Push(c.ToString());
+                continue;
+            }
+            var sb = "";
+            while (stk.Peek() != "[") {
+                sb = stk.Peek() + sb;
+                stk.Pop();
+            }
+
+            stk.Pop();
+            var digit_string = "";
+            while (stk.Count > 0 && char.IsDigit(stk.Peek()[0])) {
+                digit_string = stk.Peek() + digit_string;
+                stk.Pop();
+            }
+
+            var dig = int.Parse(digit_string);
+            var cur = sb.ToString();
+            for (int i = 0; i < dig - 1; i++) sb += (cur);
+            Console.WriteLine(sb.ToString());
+            stk.Push(sb.ToString());
+        }
+
+        var charArr = stk.ToArray();
+        Array.Reverse(charArr);
+        return string.Join("", charArr);
+    }
+}
