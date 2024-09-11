cf3a692f7ee0c3bf88bb3624318409daad74580b
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-09-11 16:03:21 -0700

    Time: 53 ms (55.77%), Space: 43.2 MB (92.31%) - LeetHub

--- 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.cs
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.cs
@@ -1,19 +1,22 @@
 public class Solution {
     public int BestClosingTime(string customers) {
-        int n = customers.Length;
-        int earliest = 0;
-        int minPenalty = 0;
-        int curPenalty = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (customers[i] == 'Y') curPenalty--;
-            else curPenalty++;
-            if (curPenalty < minPenalty) {
-                minPenalty = curPenalty;
-                earliest = i + 1;
-            }
+        
+        int total = 0; 
+        foreach(var c in customers) {
+            if (c == 'Y') total++;
+        }
+        int ans = 0, minPenalty = total;
+        for (int i = 0; i < customers.Length; i++) {
+            char c = customers[i];
+            if (c == 'Y') {
+                total--;
+                if (total < minPenalty) {
+                    ans = i + 1;
+                    minPenalty = total; 
+                }
+            } else total++;
         }
 
-        return earliest;
+        return ans;
     }
 }
