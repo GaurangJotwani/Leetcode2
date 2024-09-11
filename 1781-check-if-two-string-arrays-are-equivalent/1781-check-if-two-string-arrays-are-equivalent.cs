50cb306152d05849400745f7f9fb0641453ffc7e
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-09-10 22:18:09 -0700

    Time: 76 ms (11.28%), Space: 42.2 MB (90.71%) - LeetHub

--- 1781-check-if-two-string-arrays-are-equivalent/1781-check-if-two-string-arrays-are-equivalent.cs
+++ 1781-check-if-two-string-arrays-are-equivalent/1781-check-if-two-string-arrays-are-equivalent.cs
@@ -1,7 +1,5 @@
 public class Solution {
     public bool ArrayStringsAreEqual(string[] word1, string[] word2) {
-        string s1 = String.Join("", word1);
-        string s2 = String.Join("", word2);
-        return s1 == s2;
+        return String.Join("", word1) == String.Join("", word2);
     }
 }
