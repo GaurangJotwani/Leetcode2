e40b6f790a161a8a9bbc617d6aceeaeec1e43b31
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-09-12 09:50:29 -0700

    Time: 56 ms (90.38%), Space: 40.7 MB (42.53%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.cs
+++ 0682-baseball-game/0682-baseball-game.cs
@@ -2,20 +2,15 @@ public class Solution {
     public int CalPoints(string[] operations) {
         var stk = new Stack<int>();
         foreach (var o in operations) {
-            if (o == "C") {
-                stk.Pop();
-            } else if (o == "D") {
-                stk.Push(stk.Peek() * 2);
-            } else if (o == "+") {
+            if (o == "C") stk.Pop();
+            else if (o == "D") stk.Push(stk.Peek() * 2);
+            else if (o == "+") {
                 int num1 = stk.Peek();
                 stk.Pop();
                 int num3 = stk.Peek() + num1;
                 stk.Push(num1);
                 stk.Push(num3);
-            } else {
-                stk.Push(int.Parse(o));
-            }
-            var arr = stk.ToArray();
+            } else stk.Push(int.Parse(o));
         }
 
         int res = 0;
