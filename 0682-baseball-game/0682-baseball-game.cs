376b48f23c4d99e3204596c27758989899f06fc5
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-09-12 09:49:30 -0700

    Time: 84 ms (7.09%), Space: 48.4 MB (6.33%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public int CalPoints(string[] operations) {
+        var stk = new Stack<int>();
+        foreach (var o in operations) {
+            if (o == "C") {
+                stk.Pop();
+            } else if (o == "D") {
+                stk.Push(stk.Peek() * 2);
+            } else if (o == "+") {
+                int num1 = stk.Peek();
+                stk.Pop();
+                int num3 = stk.Peek() + num1;
+                stk.Push(num1);
+                stk.Push(num3);
+            } else {
+                stk.Push(int.Parse(o));
+            }
+            var arr = stk.ToArray();
+        }
+
+        int res = 0;
+        while (stk.Count > 0) {
+            res += stk.Peek();
+            stk.Pop();
+        }
+        return res;
+    }
+}
