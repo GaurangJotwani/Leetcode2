eb547678b6838a954aa7d3547de6bd246cab2d09
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-09-13 08:18:45 -0700

    Time: 858 ms (20.21%), Space: 142 MB (5.32%) - LeetHub

---
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public string RemoveDuplicates(string s, int k) {
+        var letters = new Stack<(char c, int freq)>();
+        foreach (char c in s) {
+            if (letters.Count > 0 && letters.Peek().c == c) {
+                var pair = letters.Peek();
+                letters.Pop();
+                if (pair.freq + 1 == k) continue;
+                else letters.Push((c, pair.freq + 1));
+            } else letters.Push((c, 1));
+        }
+        string res = "";
+        while (letters.Count > 0) {
+            var pair = letters.Peek();
+            letters.Pop();
+            for (int i = 0; i < pair.freq; i++) res = pair.c + res;
+        }
+        return res;
+    }
+}
