20138d7925f3ebe4d3583006f230c9f461ddaa25
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-09-14 12:13:50 -0700

    Time: 96 ms (12.18%), Space: 44.5 MB (45.51%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cs
+++ 0740-delete-and-earn/0740-delete-and-earn.cs
@@ -6,18 +6,13 @@ public class Solution {
         
         nums = nums.ToHashSet().ToArray();
         Array.Sort(nums);
-        
-        
+
         var dp = new int[nums.Length + 1];
         dp[1] = counts[nums[0]] * nums[0];
 
         for (int i = 2; i < dp.Length; i++) {
-            if (nums[i - 2] + 1 != nums[i - 1]) {
-                // always better to rob it
-                dp[i] = dp[i - 1] + counts[nums[i - 1]] * nums[i - 1];
-            } else {
-                dp[i] = Math.Max(dp[i - 1], dp[i - 2] + counts[nums[i - 1]] * nums[i - 1]);
-            }
+            if (nums[i - 2] + 1 != nums[i - 1]) dp[i] = dp[i - 1] + counts[nums[i - 1]] * nums[i - 1];
+            else dp[i] = Math.Max(dp[i - 1], dp[i - 2] + counts[nums[i - 1]] * nums[i - 1]);
         }
 
         return dp[nums.Length];
