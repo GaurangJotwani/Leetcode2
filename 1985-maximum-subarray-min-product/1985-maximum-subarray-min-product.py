3aa8e27f0de376d3b22caa093be2e8310c2b4a2e
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-09-15 13:01:21 -0700

    Time: 759 ms (97.11%), Space: 28.9 MB (59.73%) - LeetHub

--- 1985-maximum-subarray-min-product/1985-maximum-subarray-min-product.py
+++ 1985-maximum-subarray-min-product/1985-maximum-subarray-min-product.py
@@ -1,36 +1,16 @@
-from collections import deque
-from itertools import accumulate, chain
-from math import inf
-
-MAX = 10 ** 9 + 7
-
-
 class Solution:
-    def maxSumMinProduct(self, A: list[int]) -> int:
-        """
-        @also see https://leetcode.com/problems/maximum-subarray-min-product/solutions/1198800/python3-mono-stack/
-        :param A: 
-        :return: 
-        """
-        # prefix_sum[i] = sum(A[:i])
-        prefix_sum = [*accumulate(A, initial=0)]
-
-        # creating monotonic increasing stack
-        push, pop = (stk := deque()).append, stk.pop
-
-        output = 0
-
-        for i, a in enumerate(chain(A, [-inf])):
-            while stk and A[stk[-1]] >= a:
-                mn = A[pop()]
-
-                j = stk[-1] if stk else -1
-
-                output = max(
-                    output,
-                    mn * (prefix_sum[i] - prefix_sum[j + 1])  # mn * sum(A[j: i])
-                )
-
-            push(i)
-
-        return output % MAX
+    def maxSumMinProduct(self, nums: List[int]) -> int:
+        mod = 10 ** 9 + 7
+        ps = [0]
+        stack = [-1]
+        nums.append(0)
+        res = 0
+        
+        for i in range(len(nums)):
+            while nums[stack[-1]] > nums[i]:
+                min_val = nums[stack.pop()]
+                range_sum = ps[i] - ps[stack[-1] + 1]
+                res = max(res, range_sum * min_val)
+            stack.append(i)
+            ps.append(ps[-1] + nums[i])
+        return res % mod
