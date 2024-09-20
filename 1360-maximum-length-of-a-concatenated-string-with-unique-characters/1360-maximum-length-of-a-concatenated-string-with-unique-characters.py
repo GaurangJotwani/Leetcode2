9ba05163f90787a403e7681a2e5d1def82fbc35a
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-09-20 14:24:28 -0700

    Time: 93 ms (44.61%), Space: 16.8 MB (30.68%) - LeetHub

---
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/1360-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -0,0 +1,51 @@
+class Solution:
+    def maxLength(self, arr: List[str]) -> int:
+
+        new_arr = []
+        for word in arr:
+            s = set()
+            canAdd = True
+            for c in word:
+                if c in s:
+                    canAdd = False
+                    break
+                s.add(c)
+            if canAdd:
+                new_arr.append(word)
+        
+        print(new_arr)
+        arr = new_arr
+
+        res = [0]
+        def helper(i, curr):
+            if i == len(arr):
+                tmp = "".join(curr)
+                res[0] = max(res[0], len(tmp))
+                return
+            
+            helper(i + 1, curr)
+
+            canAdd = True
+            s = set("".join(curr))
+            for c in arr[i]:
+                if c in s:
+                    canAdd = False
+                    break
+            
+            if canAdd:
+                curr.append(arr[i])
+                helper(i + 1, curr)
+                curr.pop()
+        
+        helper(0, [])
+        return res[0]
+
+        
+
+
+
+
+
+
+
+
