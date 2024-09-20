e49af97b2160ecd48c55d22802efde42bc26b144
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-09-19 21:09:10 -0700

    Time: 2703 ms (54.32%), Space: 16.8 MB (20.79%) - LeetHub

---
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.py
@@ -0,0 +1,31 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        
+        total = sum(matchsticks)
+        n = len(matchsticks)
+        if total % 4:
+            return False
+        target = total // 4
+        matchsticks.sort(reverse=True)
+
+        used = [False for i in range(n)]
+        def dfs(i, cSum, groups):
+            if groups == 0:
+                return True
+            
+            if cSum == target:
+                return dfs(0,0,groups - 1)
+            
+            for j in range(i, n):
+                if used[j] or cSum + matchsticks[j] > target:
+                    continue
+                
+                used[j] = True
+                if dfs(j + 1,  cSum + matchsticks[j], groups):
+                    return True
+                used[j] = False
+            
+            return False
+        
+        return dfs(0, 0, 4)
+
