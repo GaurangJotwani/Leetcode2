bf0d738410600e80987f5585adc31c4dd6cb857b
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-10-09 11:25:15 -0700

    Time: 998 ms (48.06%), Space: 42.5 MB (47.09%) - LeetHub

--- 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.py
+++ 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.py
@@ -1,13 +1,19 @@
+
+# Approach
+# Sort queries and process each with a BFS (or DFS), keeping track of visited cells, 
+# to count accessible cells with values below the query.
+# Use Min Heap so that we dont have to repeat BFS
+
 class Solution:
     def maxPoints(self, grid: List[List[int]], queries: List[int]) -> List[int]:
         m, n = len(grid), len(grid[0])
         sorted_queries = sorted((q, i) for i, q in enumerate(queries))
         result = [0] * len(queries)
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        visited = [[False] * n for _ in range(m)]
+        visited = set()
         max_heap = []
         heappush(max_heap, (grid[0][0], 0, 0))
-        visited[0][0] = True
+        visited.add((0, 0))
         points = 0
 
         for q, idx in sorted_queries:
@@ -16,8 +22,8 @@ class Solution:
                 points += 1  # Add point for the cell
                 for dx, dy in directions:
                     nx, ny = x + dx, y + dy
-                    if 0 <= nx < m and 0 <= ny < n and not visited[nx][ny]:
-                        visited[nx][ny] = True
+                    if 0 <= nx < m and 0 <= ny < n and (nx,ny) not in visited:
+                        visited.add((nx,ny))
                         heappush(max_heap, (grid[nx][ny], nx, ny))
             result[idx] = points  # Store the result for each query
 
