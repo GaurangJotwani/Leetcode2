3bb57a8ee58be78a83c142c2cdc07b59100873c4
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-10-08 23:14:06 -0700

    Time: 457 ms (25.59%), Space: 17.1 MB (23.63%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
@@ -6,7 +6,7 @@ class Solution:
             if nums[right] == 0:
                 zero_count += 1
             # If zero_count exceeds k, shrink the window from the left
-            if zero_count > k:
+            while zero_count > k:
                 if nums[left] == 0:
                     zero_count -= 1
                 left += 1
