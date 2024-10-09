106ab47610602d44969d222246f642de8d5e47b5
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-10-09 14:08:48 -0700

    Time: 28 ms (90.72%), Space: 16.8 MB (9.48%) - LeetHub

---
+++ 0796-reaching-points/0796-reaching-points.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reachingPoints(self, sx: int, sy: int, tx: int, ty: int) -> bool:
+        
+        while tx >= sx and ty >= sy:
+            if tx == sx and ty == sy:
+                return True
+            if tx > ty:
+                if ty > sy:
+                    tx %= ty  # Reduce tx by multiples of ty
+                else:
+                    return (tx - sx) % ty == 0
+            else:
+                if tx > sx:
+                    ty %= tx  # Reduce ty by multiples of tx
+                else:
+                    return (ty - sy) % tx == 0
+        return False
