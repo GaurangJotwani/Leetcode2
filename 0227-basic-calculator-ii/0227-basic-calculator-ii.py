4ede84f5ac7533f950cd97053cdd9e144315930e
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-11-27 15:55:39 -0800

    Time: 103 ms (22.87%), Space: 18.6 MB (74.23%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -0,0 +1,36 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        cur = res = prev = 0
+        cur_operation = "+"
+
+        i = 0
+
+        while i < len(s):
+
+            curr_char = s[i]
+            if curr_char.isdigit():
+                while i < len(s) and s[i].isdigit():
+                    cur = cur * 10 + int(s[i])
+                    i += 1
+                i -= 1
+                if cur_operation == "+":
+                    res += cur
+                    prev = cur
+                elif cur_operation == "-":
+                    res -= cur
+                    prev = -cur
+                elif cur_operation == "*":
+                    res -= prev
+                    res += prev * cur
+                    prev = prev * cur
+                else:
+                    res -= prev
+                    res += int(prev / cur)
+                    prev = int(prev / cur)
+            elif curr_char != " ":
+                cur_operation = s[i]
+            cur = 0
+            i += 1
+        
+        return res
+
