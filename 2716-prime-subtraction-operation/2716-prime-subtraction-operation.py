3281eea1fb24477c57656a603028e4775f8916d3
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-11-29 12:20:59 -0800

    Time: 212 ms (13.34%), Space: 17.7 MB (9.36%) - LeetHub

---
+++ 2716-prime-subtraction-operation/2716-prime-subtraction-operation.py
@@ -0,0 +1,31 @@
+class Solution:
+    def primeSubOperation(self, nums: List[int]) -> bool:
+
+        list_of_primes = [0]
+        for i in range(2, 1001):
+            if self.isPrime(i):
+                list_of_primes.append(i)
+        
+        for i, num in enumerate(nums):
+            lower = nums[i - 1] if i - 1 >= 0 else float("-inf")
+            for prime in list_of_primes:
+                if prime >= num or num - prime <= lower:
+                    break
+                nums[i] = num - prime
+
+            if nums[i] <= lower:
+                return False
+        
+        print(nums)
+        return True
+        
+
+    
+    def isPrime(self, num):
+        i = 2
+        while i * i <= num:
+            if num % i == 0:
+                return False
+            i += 1
+        return True
+
