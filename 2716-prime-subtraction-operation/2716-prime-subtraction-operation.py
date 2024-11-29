1750ae3d5d74f13d2bf5a727cadbad8add9c3129
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-11-29 12:26:27 -0800

    Time: 14 ms (77%), Space: 17.6 MB (9.36%) - LeetHub

--- 2716-prime-subtraction-operation/2716-prime-subtraction-operation.py
+++ 2716-prime-subtraction-operation/2716-prime-subtraction-operation.py
@@ -1,30 +1,33 @@
 class Solution:
-    def primeSubOperation(self, nums: List[int]) -> bool:
+    def primeSubOperation(self, nums):
+        max_element = max(nums)
 
-        list_of_primes = [0]
-        for i in range(2, max(nums) + 1):
-            if self.isPrime(i):
-                list_of_primes.append(i)
-        
-        for i, num in enumerate(nums):
-            lower = nums[i - 1] if i - 1 >= 0 else float("-inf")
-            for prime in list_of_primes:
-                if prime >= num or num - prime <= lower:
-                    break
-                nums[i] = num - prime
+        # Store the sieve array.
+        sieve = [1] * (max_element + 1)
+        sieve[1] = 0
+        for i in range(2, int(math.sqrt(max_element + 1)) + 1):
+            if sieve[i] == 1:
+                for j in range(i * i, max_element + 1, i):
+                    sieve[j] = 0
 
-            if nums[i] <= lower:
+        # Start by storing the currValue as 1, and the initial index as 0.
+        curr_value = 1
+        i = 0
+        while i < len(nums):
+            # Store the difference needed to make nums[i] equal to currValue.
+            difference = nums[i] - curr_value
+
+            # If difference is less than 0, then nums[i] is already less than
+            # currValue. Return false in this case.
+            if difference < 0:
                 return False
 
+            # If the difference is prime or zero, then nums[i] can be made
+            # equal to currValue.
+            if sieve[difference] or difference == 0:
+                i += 1
+                curr_value += 1
+            else:
+                # Otherwise, try for the next currValue.
+                curr_value += 1
         return True
-        
-
-    
-    def isPrime(self, num):
-        i = 2
-        while i * i <= num:
-            if num % i == 0:
-                return False
-            i += 1
-        return True
-
