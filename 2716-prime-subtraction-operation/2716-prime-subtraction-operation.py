8531f794c92ab47709ea81f7943e85291db98da3
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-11-29 12:34:17 -0800

    Time: 102 ms (26.27%), Space: 17.2 MB (9.36%) - LeetHub

--- 2716-prime-subtraction-operation/2716-prime-subtraction-operation.py
+++ 2716-prime-subtraction-operation/2716-prime-subtraction-operation.py
@@ -1,33 +1,41 @@
 class Solution:
+    def isprime(self, n):
+        for i in range(2, isqrt(n) + 1):
+            if n % i == 0:
+                return False
+        return True
+
     def primeSubOperation(self, nums):
-        max_element = max(nums)
+        maxElement = max(nums)
 
-        # Store the sieve array.
-        sieve = [1] * (max_element + 1)
-        sieve[1] = 0
-        for i in range(2, int(math.sqrt(max_element + 1)) + 1):
-            if sieve[i] == 1:
-                for j in range(i * i, max_element + 1, i):
-                    sieve[j] = 0
+        # Store the previousPrime array.
+        previous_prime = [0] * (maxElement + 1)
+        for i in range(2, maxElement + 1):
+            if self.isprime(i):
+                previous_prime[i] = i
+            else:
+                previous_prime[i] = previous_prime[i - 1]
 
-        # Start by storing the currValue as 1, and the initial index as 0.
-        curr_value = 1
-        i = 0
-        while i < len(nums):
-            # Store the difference needed to make nums[i] equal to currValue.
-            difference = nums[i] - curr_value
+        for i in range(len(nums)):
 
-            # If difference is less than 0, then nums[i] is already less than
-            # currValue. Return false in this case.
-            if difference < 0:
+            # In case of first index, we need to find the largest prime less
+            # than nums[0].
+            if i == 0:
+                bound = nums[0]
+            else:
+                # Otherwise, we need to find the largest prime, that makes the
+                # current element closest to the previous element.
+                bound = nums[i] - nums[i - 1]
+
+            # If the bound is less than or equal to 0, then the array cannot be
+            # made strictly increasing.
+            if bound <= 0:
                 return False
 
-            # If the difference is prime or zero, then nums[i] can be made
-            # equal to currValue.
-            if sieve[difference] or difference == 0:
-                i += 1
-                curr_value += 1
-            else:
-                # Otherwise, try for the next currValue.
-                curr_value += 1
+            # Find the largest prime less than bound.
+            largest_prime = previous_prime[bound - 1]
+
+            # Subtract this value from nums[i].
+            nums[i] -= largest_prime
+
         return True
