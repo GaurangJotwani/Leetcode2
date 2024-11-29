cdebf9678718ae1b07be9f7e2bf7d35a43f7ba08
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-11-28 19:36:24 -0800

    Time: 0 ms (100%), Space: 18.5 MB (7.72%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,25 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-
-        cache = {}
-        
-        def dp(i, canBuy):
-
-            if i == len(prices):
-                return 0
-
-            if (i, canBuy) in cache:
-                return cache[(i, canBuy)]
-            
-            #dont do anything
-            p1 = dp(i + 1, canBuy)
-
-            if canBuy:
-                p2 = -prices[i] + dp(i + 1, not canBuy)
-            else:
-                p2 = prices[i] + dp(i + 1, not canBuy)
-            
-            cache[(i, canBuy)] = max(p1, p2)
-            return cache[(i, canBuy)]
-        
-        return dp(0, True)
+        maxprofit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                maxprofit += prices[i] - prices[i - 1]
+        return maxprofit
