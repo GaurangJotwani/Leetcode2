0ffc721e9ace012adbb09cd07c5963de5093499d
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-11-30 12:22:45 -0800

    Time: 179 ms (36.63%), Space: 20.5 MB (24.74%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,42 +1,25 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         
-        ROWS, COLS = len(mat), len(mat[0])
-        res = [[0] * COLS for _ in range(ROWS)]
-        
-        
         q = deque()
         visited = set()
-        
+        ROWS, COLS = len(mat), len(mat[0])
+        directions = [[1,0],[-1,0],[0,1],[0,-1]]
+
         for r in range(ROWS):
             for c in range(COLS):
-                
                 if mat[r][c] == 0:
                     q.append((r,c,0))
                     visited.add((r,c))
         
-        
-        directions = [(0, 1),(0, -1),(1, 0),(-1, 0)]
-        
         while q:
-            for _ in range(len(q)):
-                r,c,dist = q.popleft()
-                res[r][c] = dist
-                
-                for dr,dc in directions:
-                    row, col = r + dr, c + dc
-                    
-                    if (0 <= row < ROWS and
-                        0 <= col < COLS and
-                        (row,col) not in visited
-                       ):
-                        q.append((row,col, dist + 1))
-                        visited.add((row,col))
-            
+            r1,c1,d1 = q.popleft()
+            mat[r1][c1] = d1
+
+            for dr,dc in directions:
+                row,col = r1 + dr, c1 + dc
+                if 0 <= row < ROWS and 0 <= col < COLS and (row, col) not in visited:
+                    q.append((row, col, d1 + 1))
+                    visited.add((row, col))
         
-        return res
-        
-        
-        
-        
-                    
+        return mat
