024cc7c8d7e11419b30ce8b2d233439e3d715d5d
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-12-01 14:21:23 -0800

    Time: 3 ms (80.1%), Space: 17.4 MB (5.52%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,16 +1,10 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
         lst = [("M",1000),("CM", 900),("D", 500),("CD", 400),("C", 100),("XC", 90),("L", 50),("XL", 40),("X", 10),("IX", 9),("V", 5),("IV", 4),("I", 1)]
-
-
         res = []
-
-
         for symb, val in lst:
             while num >= val:
                 res.append(symb)
                 num = num - val
-                if num == 0:
-                    break
 
         return "".join(res) 
