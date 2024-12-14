055a911d37753939499d5579e3c29fe7b32fe3ea
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-12-14 13:17:25 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxTwoEvents(self, events: List[List[int]]) -> int:
+        
+        events.sort()
+        non_overlapping = []
+        overlapping = []
+        res = float("-inf")
+
+        for s,e,v in events:
+            while overlapping and s > overlapping[0][0]:
+                e1,s1,v1 = heappop(overlapping)
+                heappush(non_overlapping,(-v1,s1,e1))
+            
+            res = max(res, v)
+            if non_overlapping:
+                res = max(res, v + -1 * non_overlapping[0][0])
+            heappush(overlapping, (e,s,v))
+            print(overlapping)
+            print(non_overlapping)
+
+        return res
