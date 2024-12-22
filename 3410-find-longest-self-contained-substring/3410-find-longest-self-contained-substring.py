6ab589444ca46d64fe7056fafce4078eececf2c6
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-12-21 18:18:20 -0800

    Time: 235 ms (59.18%), Space: 18.3 MB (85.71%) - LeetHub

--- 3410-find-longest-self-contained-substring/3410-find-longest-self-contained-substring.py
+++ 3410-find-longest-self-contained-substring/3410-find-longest-self-contained-substring.py
@@ -1,18 +1,24 @@
 class Solution:
     def maxSubstringLength(self, s: str) -> int:
-        first, last = {}, {}
-        for i, c in enumerate(s):
-            if c not in first:
-                first[c] = i
-            last[c] = i
-        ans, n = -1, len(s)
-        for c, i in first.items():
-            mx = last[c]
+        first = defaultdict(int)
+        last = defaultdict(int)
+
+        for i,ch in enumerate(s):
+            if ch not in first:
+                first[ch] = i
+            last[ch] = i
+        ans = -1
+        n = len(s)
+        print(first,last)
+        for letter,i in first.items():
+            mx_rng = last[letter]
             for j in range(i, n):
-                a, b = first[s[j]], last[s[j]]
+                a,b = first[s[j]], last[s[j]]
                 if a < i:
                     break
-                mx = max(mx, b)
-                if mx == j and j - i + 1 < n:
+                mx_rng = max(mx_rng, b)
+                if mx_rng == j and j - i + 1 < n:
                     ans = max(ans, j - i + 1)
+        
         return ans
+                
