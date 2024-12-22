f6c480a4f160094f585b082e710323587ba04896
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-12-22 15:10:45 -0800

    Time: 67 ms (16.8%), Space: 19.8 MB (5.08%) - LeetHub

--- 0694-number-of-distinct-islands/0694-number-of-distinct-islands.py
+++ 0694-number-of-distinct-islands/0694-number-of-distinct-islands.py
@@ -1,55 +1,41 @@
 class Solution:
     def numDistinctIslands(self, grid: List[List[int]]) -> int:
         
-        left_most = []
-        visited = set()
-        ROWS, COLS = len(grid), len(grid[0])
-        directions = [[0,1],[0,-1],[1,0],[-1,0]]
-
-        def dfs(r,c):
-            visited.add((r,c))
-            for dr,dc in directions:
-                row,col = r + dr, c + dc
-                if not self.isOutOfBoundsOrZero(row, col, grid) and (row, col) not in visited:
-                    dfs(row, col)
-        
-        total = 0
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == 1 and (r,c) not in visited:
-                    left_most.append((r,c))
-                    total += 1
-                    dfs(r,c)
-        
-        already_done = set()
-        for i in range(len(left_most)):
-            r1,c1 = left_most[i]
-            if (r1,c1) in already_done:
-                continue
-            for j in range(i + 1, len(left_most)):
-                r2,c2 = left_most[j]
-                seen = set()
-                if self.areSame(r1, c1, r2, c2, grid, seen):
-                    total -= 1
-                    already_done.add((r2,c2))
-
-        return total
-    
-
-
-    def areSame(self, r1, c1, r2, c2, matrix, visited):
-        if (r1,c1) in visited:
+        def current_island_is_unique():
+            for other_island in unique_islands:
+                if len(other_island) != len(current_island):
+                    continue
+                for cell_1, cell_2 in zip(current_island, other_island):
+                    if cell_1 != cell_2:
+                        break
+                else:
+                    return False
             return True
-        if self.isOutOfBoundsOrZero(r1, c1, matrix) and self.isOutOfBoundsOrZero(r2, c2, matrix):
-            return True
-        if self.isOutOfBoundsOrZero(r1, c1, matrix) or self.isOutOfBoundsOrZero(r2, c2, matrix):
-            return False
-        visited.add((r1,c1))
+            
+        # Do a DFS to find all cells in the current island.
+        def dfs(row, col):
+            if row < 0 or col < 0 or row >= len(grid) or col >= len(grid[0]):
+                return
+            if (row, col) in seen or not grid[row][col]:
+                return
+            seen.add((row, col))
+            current_island.append((row - row_origin, col - col_origin))
+            dfs(row + 1, col)
+            dfs(row - 1, col)
+            dfs(row, col + 1)
+            dfs(row, col - 1)
         
-        return (self.areSame(r1 + 1, c1, r2 + 1, c2, matrix, visited) and 
-                self.areSame(r1 - 1, c1, r2 - 1, c2, matrix, visited) and 
-                self.areSame(r1, c1 - 1, r2, c2 - 1, matrix, visited) and
-                self.areSame(r1, c1 + 1, r2, c2 + 1, matrix, visited))
-    
-    def isOutOfBoundsOrZero(self,r,c,matrix):
-        return r >= len(matrix) or r< 0 or c < 0 or c >= len(matrix[0]) or matrix[r][c] == 0
+        # Repeatedly start DFS's as long as there are islands remaining.
+        seen = set()
+        unique_islands = []
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                current_island = []
+                row_origin = row
+                col_origin = col
+                dfs(row, col)
+                if not current_island or not current_island_is_unique():
+                    continue
+                unique_islands.append(current_island)
+        print(unique_islands)
+        return len(unique_islands)
