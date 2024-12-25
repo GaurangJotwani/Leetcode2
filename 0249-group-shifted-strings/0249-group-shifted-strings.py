4bbf188757a8e540feb8f3ca1e2b179cbd66767f
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-12-25 11:45:28 -0800

    Time: 3 ms (47.54%), Space: 17.8 MB (6.76%) - LeetHub

--- 0249-group-shifted-strings/0249-group-shifted-strings.py
+++ 0249-group-shifted-strings/0249-group-shifted-strings.py
@@ -2,10 +2,8 @@ class Solution:
     def groupStrings(self, strings: List[str]) -> List[List[str]]:
         patterns = defaultdict(list)
         for s in strings:
-            pattern = ""
-            if len(s) == 1:
-                patterns["0"].append(s)
-                continue
+            pattern = "0-"
+
             
             for i in range(len(s) - 1):
                 if ord(s[i + 1]) >= ord(s[i]):
