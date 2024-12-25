9dc85dcd4d91b296d154559f2a311d6f291f8b2e
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-12-25 11:46:57 -0800

    Time: 0 ms (100%), Space: 18 MB (6.76%) - LeetHub

--- 0249-group-shifted-strings/0249-group-shifted-strings.py
+++ 0249-group-shifted-strings/0249-group-shifted-strings.py
@@ -2,16 +2,15 @@ class Solution:
     def groupStrings(self, strings: List[str]) -> List[List[str]]:
         patterns = defaultdict(list)
         for s in strings:
-            pattern = "0-"
-
-            
+            pattern = ["0-"]
             for i in range(len(s) - 1):
                 if ord(s[i + 1]) >= ord(s[i]):
-                    pattern += str(ord(s[i + 1]) - ord(s[i])) + "-"
+                    pattern.append(str(ord(s[i + 1]) - ord(s[i])))
                 else:
-                    pattern += str(ord(s[i + 1]) + 26 - ord(s[i])) + "-"
+                    pattern.append(str(ord(s[i + 1]) + 26 - ord(s[i])))
+                pattern.append("-")
 
-            patterns[pattern].append(s)
+            patterns["".join(pattern)].append(s)
         
         res = []
         for key,val in patterns.items():
