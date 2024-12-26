b7e38ecf4fdcd4544696879b001e941d2806a191
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-12-26 08:58:55 -0800

    Time: 0 ms (100%), Space: 8 MB (29.75%) - LeetHub

---
+++ 2383-add-two-integers/2383-add-two-integers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        
+        while (num2 != 0) {
+            int tmp = (num1 & num2) << 1;
+            num1 = num1 ^ num2;
+            num2 = tmp;
+        }
+
+        return num1;
+    }
+};
