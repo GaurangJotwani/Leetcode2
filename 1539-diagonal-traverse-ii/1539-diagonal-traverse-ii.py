3c0ad54ab0f4531c730bf1e9867f574cb986dea1
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-12-26 10:48:33 -0800

    Time: 47 ms (88.19%), Space: 31.9 MB (69.21%) - LeetHub

--- 1539-diagonal-traverse-ii/1539-diagonal-traverse-ii.py
+++ 1539-diagonal-traverse-ii/1539-diagonal-traverse-ii.py
@@ -2,11 +2,9 @@ class Solution:
     def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
         
         q = deque()
-        visited = set()
         ROWS = len(nums)
 
         q.append((0,0))
-        visited.add((0,0))
         res = []
 
         while q:
@@ -16,12 +14,10 @@ class Solution:
             row = nums[r]
 
             if c == 0:
-                if r + 1 < ROWS and c < len(nums[r + 1]) and (r + 1, c) not in visited:
-                    visited.add((r + 1, c))
+                if r + 1 < ROWS and c < len(nums[r + 1]):
                     q.append((r + 1, c))
             
             if c + 1 < len(row):
-                visited.add((r, c + 1))
                 q.append((r, c + 1))
         
         return res
