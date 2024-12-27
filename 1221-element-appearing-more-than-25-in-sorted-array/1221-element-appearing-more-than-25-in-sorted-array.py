56221a1a9fdd823376107fa3181167154d7b8508
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-12-27 10:15:14 -0800

    Time: 3 ms (49.09%), Space: 18.8 MB (16.39%) - LeetHub

---
+++ 1221-element-appearing-more-than-25-in-sorted-array/1221-element-appearing-more-than-25-in-sorted-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findSpecialInteger(self, arr: List[int]) -> int:
+
+        n = len(arr)
+        freq = defaultdict(int)
+
+        for num in arr:
+            freq[num] += 1
+            if (freq[num] / n) * 100 > 25:
+                return num
+                
+        
