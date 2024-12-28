276d506b1f1c590f95b4cf6b091664f92cbeb265
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-12-28 08:42:15 -0800

    Time: 48 ms (94.75%), Space: 23.3 MB (10.74%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.py
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.py
@@ -1,7 +1,6 @@
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
         
-        max_seen = (values[0],0)
         test = values[0]
         res = 0
 
@@ -9,8 +8,7 @@ class Solution:
         for i in range(1, len(values)):
             num = values[i]
             res =  max(res, test + num - i)
-            if num + i >= max_seen[0] + max_seen[1]:
-                max_seen = (num, i)
+            if num + i >= test:
                 test = num + i
         
         return res
