bcb7b353cb3dace4549394c72719a62806f1d770
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-12-28 07:25:09 -0800

    Time: 0 ms (100%), Space: 17.6 MB (5.58%) - LeetHub

--- 0246-strobogrammatic-number/0246-strobogrammatic-number.py
+++ 0246-strobogrammatic-number/0246-strobogrammatic-number.py
@@ -3,28 +3,21 @@ class Solution:
 
         l = 0
         r = len(num) - 1
-
         while l <= r:
             if l == r:
                 if num[l] not in ["0", "8", "1"]:
                     return False
                 else:
                     return True
-            
             if (num[l] == "6" and num[r] == "9") or (num[r] == "6" and num[l] == "9"):
                 l += 1
                 r -= 1
                 continue
-
-            if num[l] != num[r]:
-                return False
-            
-            if num[l] in ["0", "8", "1"]:
+            if num[l] == num[r] and num[l] in ["0", "8", "1"]:
                 l += 1
                 r -= 1
             else:
                 return False
-        
         return True
             
         
