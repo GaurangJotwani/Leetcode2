2a23dd2711da3bc9e001fe7826b0ff7dc8822aec
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-12-28 07:47:56 -0800

    Time: 99 ms (9.18%), Space: 29.9 MB (13.15%) - LeetHub

---
+++ 0247-strobogrammatic-number-ii/0247-strobogrammatic-number-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findStrobogrammatic(self, n: int) -> List[str]:
+
+        res = []
+        def dfs(root):
+            if len(root) > n:
+                return
+            if len(root) == n:
+                if root[0] == "0" and n > 1:
+                    return 
+                res.append(root)
+                return
+            dfs("0" + root + "0")
+            dfs("1" + root + "1")
+            dfs("8" + root + "8")
+            dfs("6" + root + "9")
+            dfs("9" + root + "6")
+        
+        dfs("0")
+        dfs("1")
+        dfs("8")
+        dfs("00")
+        dfs("11")
+        dfs("88")
+        dfs("69")
+        dfs("96")
+        res.sort()
+        return res
