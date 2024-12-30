679f8bd9edb369e49323e916a47def5ded354ce7
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-12-30 08:15:34 -0800

    Time: 133 ms (22.85%), Space: 22.8 MB (13.06%) - LeetHub

--- 0305-number-of-islands-ii/0305-number-of-islands-ii.py
+++ 0305-number-of-islands-ii/0305-number-of-islands-ii.py
@@ -1,59 +1,75 @@
+
 class UnionFind:
+
     def __init__(self, m, n):
-        self.parent = {}
-        self.rank = {}
-    
-    def add(self, r, c):
-        if (r, c) not in self.parent:
-            self.parent[(r, c)] = (r, c)
-            self.rank[(r, c)] = 0
-    
+        self.ranks = [[1] * n for _ in range(m)]
+        self.parents = [[0] * n for _ in range(m)]
+
+        for r in range(m):
+            for c in range(n):
+                self.parents[r][c] = (r, c)
+
     def find(self, r, c):
-        if self.parent[(r, c)] != (r, c):
-            self.parent[(r, c)] = self.find(*self.parent[(r, c)])  # Path compression
-        return self.parent[(r, c)]
-    
+        if self.parents[r][c] == (r, c):
+            return (r,c)
+        tmp = self.find(self.parents[r][c][0],self.parents[r][c][1])
+        self.parents[r][c] = tmp
+        return tmp
+
     def union(self, r1, c1, r2, c2):
-        root1 = self.find(r1, c1)
-        root2 = self.find(r2, c2)
-        
-        if root1 == root2:
+
+        p1 = self.find(r1,c1)
+        p2 = self.find(r2,c2)
+       
+        if p1 == p2:
             return False
         
-        if self.rank[root1] > self.rank[root2]:
-            self.parent[root2] = root1
-        elif self.rank[root1] < self.rank[root2]:
-            self.parent[root1] = root2
+        r1, c1 = p1
+        r2, c2 = p2
+        
+        if self.ranks[r1][c1] > self.ranks[r2][c2]:
+            self.parents[r2][c2] = self.parents[r1][c1]
+            self.ranks[r1][c1] += self.ranks[r2][c2]
         else:
-            self.parent[root2] = root1
-            self.rank[root1] += 1
+            self.parents[r1][c1] = self.parents[r2][c2]
+            self.ranks[r2][c2] += self.ranks[r1][c1]
         
         return True
+        
+        
+        
+    
+
 
 
 class Solution:
     def numIslands2(self, m: int, n: int, positions: List[List[int]]) -> List[int]:
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        uf = UnionFind(m, n)
+
         visited = set()
-        num_islands = 0
-        result = []
-        
-        for r, c in positions:
-            if (r, c) in visited:
-                result.append(num_islands)
+        directions = [[1,0],[-1,0],[0,1],[0,-1]]
+        union_find = UnionFind(m, n)
+
+        number_of_components = 0
+        answer = []
+
+        for r,c in positions:
+
+            if (r,c) in visited:
+                answer.append(number_of_components)
                 continue
-            
-            uf.add(r, c)
-            visited.add((r, c))
-            num_islands += 1
-            
+
+            nearby_islands = set()
             for dr, dc in directions:
-                nr, nc = r + dr, c + dc
-                if (nr, nc) in visited:
-                    if uf.union(r, c, nr, nc):
-                        num_islands -= 1
+                row, col = r + dr, c + dc
+                if 0 <= row < m and 0 <= col < n and (row, col) in visited:
+                    nearby_islands.add(union_find.find(row, col))
             
-            result.append(num_islands)
+            number_of_components += 1
+            number_of_components -= len(nearby_islands)
+            for r1, c1 in nearby_islands:
+                union_find.union(r1, c1, r, c)
+            visited.add((r, c))
+            answer.append(number_of_components)
+        return answer
+
         
-        return result
