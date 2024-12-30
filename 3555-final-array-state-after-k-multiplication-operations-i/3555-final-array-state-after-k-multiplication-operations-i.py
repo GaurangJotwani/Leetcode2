36c186123b919f02ac82a1244e209012f9572f9b
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-12-29 18:19:27 -0800

    Time: 7 ms (16.09%), Space: 17.7 MB (7.07%) - LeetHub

---
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.py
@@ -0,0 +1,21 @@
+class Solution:
+    def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+
+
+        min_heap = []
+        for i,num in enumerate(nums):
+            heappush(min_heap, (num, i))
+        
+
+        for _ in range(k):
+            num, i = heappop(min_heap)
+            heappush(min_heap, (num * multiplier, i))
+        
+        while min_heap:
+            num, i = heappop(min_heap)
+            nums[i] = num
+        
+        return nums
+
+            
+        
