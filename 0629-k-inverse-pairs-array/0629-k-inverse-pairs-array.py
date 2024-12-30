c002116b359967eaffa63c82e00e01c307caa134
Gaurang Jotwani <48160866+GaurangJotwani@users.noreply.github.com>
2024-12-30 11:43:28 -0800

    Time: 428 ms (57.07%), Space: 25.8 MB (63.9%) - LeetHub

--- 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.py
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.py
@@ -5,13 +5,30 @@ class Solution:
         dp[0][0] = 1
         mod = 10**9 + 7
 
-        for i in range(1, n  + 1):
-            for j in range(k + 1):
-                dp[i][j] = dp[i - 1][j]
-                if j - 1 >= 0:
-                    dp[i][j] += dp[i][j - 1]
-                if j - i >= 0:
-                    dp[i][j] -= dp[i-1][j - i]
+        # for i in range(1, n  + 1):
+        #     for j in range(k + 1):
+        #         dp[i][j] = dp[i - 1][j]
+        #         if j - 1 >= 0:
+        #             dp[i][j] += dp[i][j - 1]
+        #         if j - i >= 0:
+        #             dp[i][j] -= dp[i-1][j - i]
+
+        curr = [0] * (k + 1)
+        prev = [0] * (k + 1)
+        prev[0] = 1
         
-        return dp[n][k] % mod
+        for i in range(1, n + 1):
+            for j in range(k + 1):
+                curr[j] = prev[j]
+                if j - 1 >= 0:
+                    curr[j] += curr[j - 1]
+                    curr[j] = curr[j] % mod
+                if j - i >= 0:
+                    curr[j] -= prev[j - i]
+            
+            tmp = prev
+            prev = curr
+            curr = tmp
+        
+        return prev[k] % mod
 
